8ef2937e45573f40aba0ba25e7005b8066bb768f
Diana Huang <dkh@edx.org>
2015-03-17 14:18:22 -0400

    Access LMS endpoint, not Oscar endpoint.

--- locust/ecommerce/locustfile.py
+++ locust/ecommerce/locustfile.py
@@ -1,18 +1,19 @@
 """ Locust file for testing the ecommerce orders endpoint. """
 import json
 import os
-import jwt
+import uuid
 from locust import HttpLocust, TaskSet, task
 
-
-# get the secret from environment variables
-# Note: the JWT_SECRET must be set for this file to run correctly.
-JWT_SECRET = os.environ['JWT_SECRET']
 
 # Basic auth
 BASIC_AUTH_CREDENTIALS = None
 if 'BASIC_AUTH_USER' in os.environ and 'BASIC_AUTH_PASSWORD' in os.environ:
     BASIC_AUTH_CREDENTIALS = (os.environ['BASIC_AUTH_USER'], os.environ['BASIC_AUTH_PASSWORD'])
+
+EMAIL_USERNAME = "success"
+EMAIL_URL = "simulator.amazonses.com"
+# Set a password for the users
+USER_PASSWORD = "test"
 
 
 class ErrorResponse(Exception):
@@ -22,57 +23,76 @@ class ErrorResponse(Exception):
     pass
 
 
-class PurchaseEndpointTasks(TaskSet):
+class AutoAuthTaskSet(TaskSet):
+    """Use the auto-auth end-point to create test users. """
+
+    def auto_auth(self):
+        """Create a new account with given credentials and log in.
+
+        This requires that the test server has the feature flag
+        `AUTOMATIC_AUTH_FOR_TESTING` set to True.
+
+        """
+        del self.client.cookies["sessionid"]
+        self.username = "{0}{1}".format(EMAIL_USERNAME, uuid.uuid4().hex[:20])
+        self.full_email = "{0}@{1}".format(self.username, EMAIL_URL)
+        params = {
+            'password': USER_PASSWORD,
+            'email': self.full_email,
+            'username': self.username,
+        }
+        print "AUTO AUTHING"
+        self.client.get("/auto_auth", params=params, verify=False, name="/auto_auth")
+        return self.username
+
+
+class PurchaseEndpointTasks(AutoAuthTaskSet):
 
     """ Load tests for the purchase endpont.
 
-    Notes for running this test:
-        The expectation of the ecommerce endpoint is that users will be created
-        from this token if they do not already exist. There are two methods you can
-        use to keep your results consistent.
-
-        Method 1: Clear out the DB of all new data between each run of the test.
-        Method 2: Run the test once to generate a set of users in the DB.
-                  Discard the results of this test. All tests after this should
-                  be consistent because the users have all been created in the DB.
-
+    Notes:
+        These tests use the auto_auth endpoint on the LMS to generate unique users who will then
+        be created on the ecommerce service.
     """
 
-    COURSE_SKU = "SEAT-HONOR-EDX-DEMOX-DEMO-COURSE"
-    user_id = 1
+    COURSE_ID = "edX/DemoX/Demo_Course"
 
     def on_start(self):
         """ Set up basic auth and check for the JWT secret key. """
         if BASIC_AUTH_CREDENTIALS is not None:
             self.client.auth = BASIC_AUTH_CREDENTIALS
 
-    def _generate_user_token(self):
+        self.auto_auth()
+
+    @property
+    def _post_headers(self):
+        """Headers for a POST request, including the CSRF token. """
+        return {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRFToken': self.client.cookies.get('csrftoken', ''),
+            'Referer': self.locust.host
+        }
+
+    def _post(self, *args, **kwargs):
+        """Make a POST request to the server.
+
+        Skips SSL verification and sends the CSRF cookie.
+
+        Raises a NotAuthorizedException if the server responds
+        with a status 403.
         """
-        Return a JWT token for a unique user and user email combination.
-
-        Args:
-            None
-
-        Returns:
-            the JWT token that represents a unique username and email
-        """
-        user_base = 'edx'
-        user = "{}-{}".format(user_base, self.user_id)
-        user_email = "{}@example.com".format(user)
-        token = jwt.encode({'username': user, 'email': user_email}, JWT_SECRET)
-
-        self.user_id = self.user_id + 1
-        return token
+        kwargs['verify'] = False
+        kwargs['headers'] = self._post_headers
+        response = self.client.post(*args, **kwargs)
+        return response
 
     @task(1)
     def purchase(self):
         """ Test the endpoint for purchasing specific courses. """
-        data = {"sku": self.COURSE_SKU}
-        headers = {
-            'content-type': 'application/json',
-            'Authorization': 'JWT ' + self._generate_user_token()
-        }
-        resp = self.client.post('/api/v1/orders/', data=json.dumps(data), headers=headers)
+        data = {"course_id": self.COURSE_ID}
+        resp = self._post('/commerce/orders/', data=json.dumps(data))
+        print "status code: {}".format(resp.status_code)
         if resp.status_code != 200:
             raise ErrorResponse
 
