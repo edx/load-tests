9da54a64cefa664658a870897c4dd43506056e87
Renzo Lucioni <renzo@renzolucioni.com>
2015-04-18 18:45:12 -0400

    Clean up and update ecommerce load tests
    Includes Locust tasks for exercising the basket creation endpoint. Removes duplicated and unnecessary files.

---
+++ locust/ecommerce/baskets.py
@@ -0,0 +1,24 @@
+"""Locust tests related to basket operations."""
+import os
+import json
+
+from locust import task
+
+import config
+from ecommerce import EcommerceTasks
+
+
+class BasketsTasks(EcommerceTasks):
+    """Contains tasks which exercise basket-related behavior."""
+
+    @task
+    def purchase_single_free_product(self):
+        """Simulate the purchase of a single free product via the LMS."""
+        # If a user tries to purchase the same product more than once, we'll get 409s from
+        # the LMS. To avoid this situation, we create a new user each time this task is called.
+        self.auto_auth()
+
+        post_data = {
+            'course_id': config.COURSE_ID,
+        }
+        self.post('/commerce/baskets/', data=json.dumps(post_data), name='purchase_single_free_product')
---
+++ locust/ecommerce/config.py
@@ -0,0 +1,5 @@
+"""Configuration shared across ecommerce tasks."""
+
+
+# ID of a course on the LMS with a SKU on its honor mode
+COURSE_ID = 'edX/DemoX.1/2014'
