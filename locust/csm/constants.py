7ed8334591693965ccaacfd4487284670c892945
Ali Mohammad <alawibaba@gmail.com>
2015-08-13 11:25:43 -0400

    Use the DjangoXBlockUserStateClient interface.

--- locust/csm/constants.py
+++
@@ -1,5 +0,0 @@
-""" Constants used within tests """
-
-QUESTION_POST_DATA = {
-    '/courses/MITx/15.071x_2/1T2015/xblock/i4x:;_;_MITx;_15.071x_2;_problem;_c5a0ac6bd6744916ab519c38d4276d4f/handler/xmodule_handler/problem_check': {'input_i4x-MITx-15_071x_2-problem-c5a0ac6bd6744916ab519c38d4276d4f_2_1[]':'choice_0', 'input_i4x-MITx-15_071x_2-problem-c5a0ac6bd6744916ab519c38d4276d4f_2_1[]':'choice_3', 'input_i4x-MITx-15_071x_2-problem-c5a0ac6bd6744916ab519c38d4276d4f_3_1[]':'choice_1', 'input_i4x-MITx-15_071x_2-problem-c5a0ac6bd6744916ab519c38d4276d4f_3_1[]':'choice_2'}
-}
--- locust/csm/locustfile.py
+++ locust/csm/locustfile.py
@@ -7,121 +7,77 @@ import random
 import sys
 import time
 
-from locust import HttpLocust, TaskSet, task, events
+from locust import Locust, TaskSet, task, events
 
 # Work around the fact that Locust runs locustfiles as scripts within packages
 # and therefore doesn't allow the use of absolute or explicit relative imports.
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'helpers'))
-import constants
 
-# Basic auth
-BASIC_AUTH_CREDENTIALS = None
-if 'BASIC_AUTH_USER' in os.environ and 'BASIC_AUTH_PASSWORD' in os.environ:
-    BASIC_AUTH_CREDENTIALS = (os.environ['BASIC_AUTH_USER'],
-                              os.environ['BASIC_AUTH_PASSWORD'])
+sys.path.append("/home/alawi/edx/orphans/edx-platform/lms")
+sys.path.append("/home/alawi/edx/orphans/edx-platform/lms/djangoapps")
+sys.path.append("/home/alawi/edx/orphans/edx-platform/common/djangoapps")
+sys.path.append("/home/alawi/bin/opaque-keys/")
 
-# Existing user in the LMS
-LMS_USER_EMAIL = os.environ.get('LMS_USER_EMAIL')
-LMS_USER_PASSWORD = os.environ.get('LMS_USER_PASSWORD')
+from opaque_keys.edx.locator import BlockUsageLocator
 
-class LMSPage(object):
-    """Base class for page objects. """
-
-    def __init__(self, hostname, client):
-        """Initialize the client.
-
-        Arguments:
-            hostname (unicode): The hostname of the test server, sent as the "Referer" HTTP header.
-            client (Session): The test client used by locust.
-
-        """
-        self.hostname = hostname
-        self.client = client
-
-        if BASIC_AUTH_CREDENTIALS is not None:
-            self.client.auth = BASIC_AUTH_CREDENTIALS
-
-    def get(self, url):
-        """HTTP GET request.
-
-        Disables SSL verification, which may not be set up in the test environment.
-
-        Arguments:
-            url (unicode): The URL end-point used to make the request.
-
-        Returns:
-            Response
-
-        """
-        return self.client.get(url, verify=False)
-
-    def post(self, url, params):
-        """HTTP POST request.
-
-        Includes CSRF token header and disables SSL verification.
-
-        Arguments:
-            url (unicode): The URL end-point used to make the request.
-            params (dict): The POST params to send with the request.
-
-        Returns:
-            Response
-
-        """
-        return self.client.post(
-            url,
-            params,
-            headers=self._post_headers,
-            verify=False
-        )
-
-    def login(self, email, password, remember=False):
-        """Log in using the login page. """
-        params = {
-            "email": email,
-            "password": password,
-            "remember": "true" if remember else "false",
-        }
-        response = self.post("/login_ajax", params)
-        print response.text
-
-    def post_response(self, path, payload):
-        self.post(path, payload)
-
-    @property
-    def _post_headers(self):
-        """Headers for a POST request, including the CSRF token. """
-        return {
-            'Content-type': 'application/json',
-            'Accept': 'application/json',
-            'X-CSRFToken': self.client.cookies.get('csrftoken', ''),
-            'Referer': self.hostname
-        }
-
+os.environ["DJANGO_SETTINGS_MODULE"] = "lms.envs.devplus"
 
 class QuestionResponse(TaskSet):
     "Respond to a question in the LMS."
 
-    def on_start(self):
-        "Setup method; log in to the LMS."
+    @task
+    def set_many(self):
+        "set many load test"
 
-        self.page = LMSPage(self.locust.host, self.client)
-        if BASIC_AUTH_CREDENTIALS:
-            self.client.auth = BASIC_AUTH_CREDENTIALS
-        self.client.get("/")
-        self.page.login(LMS_USER_EMAIL, LMS_USER_PASSWORD)
+        usage = BlockUsageLocator.from_string('block-v1:HarvardX+SPU27x+2015_Q2+type@html+block@1a1866accf254461aa2df3e0b4238a5f')
+        start_time = time.time()
+        try:
+            self.client.set_many(self.client.user.username, {usage: {"soup": "delicious"}})
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="", name="set_many", response_time=total_time, exception=e)
+        else:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="", name="set_many", response_time=total_time, response_length=0)
 
     @task
-    def respond_to_question(self):
-        "Respond to question."
-        uri = random.choice(constants.QUESTION_POST_DATA.keys())
-        payload = constants.QUESTION_POST_DATA[uri]
-        self.page.post_response(uri, payload)
+    def get_many(self):
+        "get many load test"
 
+        usage = BlockUsageLocator.from_string('block-v1:HarvardX+SPU27x+2015_Q2+type@html+block@1a1866accf254461aa2df3e0b4238a5f')
+        start_time = time.time()
+        try:
+            [s for s in self.client.get_many(self.client.user.username, [usage])]
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="", name="get_many", response_time=total_time, exception=e)
+        else:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="", name="get_many", response_time=total_time, response_length=0)
 
-class WebsiteUser(HttpLocust):
-    "Locust user class."
+class UserStateClientClient(Locust):
+    "Locust class for the User State Client."
 
     task_set = QuestionResponse
-    min_wait = 10
-    max_wait = 50
+    min_wait = 1000
+    max_wait = 5000
+
+    def __init__(self):
+        super(UserStateClientClient, self).__init__()
+        if self.host is None:
+            raise LocustError("You must specify the base host. Either in the host attribute in the Locust class, or on the command line using the --host option.")
+
+        from django.conf import settings
+        settings.DATABASES = {
+                'default': {
+                'HOST': self.host,
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'edxapp',
+                'USER': 'edxapp',
+                'PASSWORD': 'password',
+                'PORT': '3306',
+            }
+        }
+        import courseware.user_state_client as user_state_client
+        from student.tests.factories import UserFactory
+
+        self.client = user_state_client.DjangoXBlockUserStateClient(user=UserFactory.create())
