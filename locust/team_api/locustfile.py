3241cda4fcf6d4ed28c7d20f97c27d04e0a21c3c
Eric Fischer <efischer@edx.org>
2015-08-27 09:43:45 -0400

    Updating weights and waits
    Updating values of `LOCUST_MIN_WAIT`, `LOCUST_MAX_WAIT`, and
    relative task weights to roughly correspond to new relic data for
    prod-edx-forum.
    
    TNL-3133

--- locust/team_api/locustfile.py
+++ locust/team_api/locustfile.py
@@ -257,39 +257,39 @@ class BaseTeamsTask(EdxAppTasks):
 class TeamAPITasks(BaseTeamsTask):
     """Create and edit teams."""
 
-    @task(5)
+    @task(10)
     def create_team(self):
         self._create_team()
 
-    @task(2)
+    @task(1)
     def update_team(self):
         self._update_team()
 
-    @task(5)
+    @task(120)
     def list_teams(self):
         self._list_teams()
 
-    @task(2)
+    @task(40)
     def list_teams_for_topic(self):
         self._list_teams_for_topic()
 
-    @task(2)
+    @task(50)
     def search_teams(self):
         self._search_teams()
 
-    @task(5)
+    @task(50)
     def search_teams_for_topic(self):
         self._search_teams_for_topic()
 
-    @task(8)
+    @task(200)
     def team_detail(self):
         self._team_detail()
 
-    @task(5)
+    @task(40)
     def list_topics(self):
         self._list_topics()
 
-    @task(8)
+    @task(120)
     def topic_detail(self):
         self._topic_detail()
 
@@ -300,5 +300,18 @@ class TeamAPITasks(BaseTeamsTask):
 
 class TeamLocust(HttpLocust):
     task_set = globals()[os.getenv('LOCUST_TASK_SET', 'TeamAPITasks')]
-    min_wait = int(os.getenv('LOCUST_MIN_WAIT', 7500))
-    max_wait = int(os.getenv('LOCUST_MAX_WAIT', 15000))
+
+    """
+    Wait times were chosen using this formula to find an average, then +/- 20%
+    desired requests per sec = (number of users) * 1000 / (average wait in ms)
+
+    The assumed values are 15 rps = 100 * 1000 / 6667.
+    If more or less than 100 users are desired, scale the wait time by the same
+    factor to hold the rps steady, or choose a different factor if changing rps
+
+    These numbers were chosen to be roughly double the daytime throughput of
+    Forums (450 rpm = 7.5 rps), as reported on New Relic for prod-edx-forum.
+    """
+
+    min_wait = int(os.getenv('LOCUST_MIN_WAIT', 5333))
+    max_wait = int(os.getenv('LOCUST_MAX_WAIT', 8000))
